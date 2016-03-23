2969f3b42c9e65aa8f7149b17b61f435cd9f60c2
Ungsik yun <ungsik.yun@gmail.com>
2016-03-24 02:05:16 +0900

    지식인 검색 추가.

--- bernard_bot/plugins/news.py
+++ bernard_bot/plugins/news.py
@@ -7,8 +7,7 @@ import re
 import random
 
 
-# @listen_to("!주사위 (\d*)?d?(\d+)?", re.IGNORECASE)
-@listen_to("!news (.*)", re.IGNORECASE)
+@listen_to("!뉴스 (.*)", re.IGNORECASE)
 def search_news(msg, q):
     url = "https://openapi.naver.com/v1/search/news.xml?query="
     url += '"' + q + '"'
@@ -24,5 +23,23 @@ def search_news(msg, q):
     for i in parsed['rss']['channel']['item']:
         msg.reply(i['title'])
         msg.reply(i['link'])
-        msg.reply(i['originallink'])
+        # msg.reply(i['originallink'])
 
+
+@listen_to("!지식인 (.*)", re.IGNORECASE)
+def search_news(msg, q):
+    url = "https://openapi.naver.com/v1/search/kin.xml?query="
+    url += '"' + q + '"'
+    headers = {
+        'X-Naver-Client-Id': os.environ['naver_client_id'],
+        'X-Naver-Client-Secret': os.environ['naver_client_secret']
+    }
+
+    r = requests.get(url, headers=headers)
+    result = r.text
+    parsed = xmltodict.parse(result)
+
+    for i in parsed['rss']['channel']['item']:
+        msg.reply(i['title'])
+        msg.reply(i['link'])
+        msg.reply(i['originallink'])
